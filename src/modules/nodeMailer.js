3c396cd5875f3a7ac8eb89f54e5030b4c1f13330
TangramMath <tangram.mb@gmail.com>
2022-11-23 10:36:34 -0300

    Update nodeMailer.js

--- src/modules/nodeMailer.js
+++ src/modules/nodeMailer.js
@@ -4,7 +4,7 @@ const nodemailer = require('nodemailer');
 const transport = nodemailer.createTransport(
   {
     host: "smtp.mailtrap.io",
-    port: 465,
+    port: 2525,
     secure: false,
     auth: {
       user: (process.env['MAIL_USER']).toString(),
@@ -21,7 +21,7 @@ async function SendToken(token, email) {
     const mailSend = await transport.sendMail({
       text: `${token}`,
       subject: 'Nothing',
-      from: 'Matheus <tangram.mb@gmail.com',
+      from: 'Matheus <matheusborgesfig@gmail.com',
       to: `${email}`
     }, err => console.log(err))
   } catch {
