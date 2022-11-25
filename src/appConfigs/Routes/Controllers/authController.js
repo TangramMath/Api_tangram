ecabf723f982183565b123c960d5961be2aa7ab9
TangramMath <tangram.mb@gmail.com>
2022-11-25 11:43:30 -0300

    Update authController.js

--- src/appConfigs/Routes/Controllers/authController.js
+++ src/appConfigs/Routes/Controllers/authController.js
@@ -4,56 +4,63 @@ const express = require('express');
 
 const router = express.Router();
 
-router.post('/signup/', async (req, res) => {
+router.post('/signup/', (req, res) => {
     try {
         console.log(req.headers, req.body);
-        const token = await useUser.create(req.body);
-        res.status(200).send(token)
+        useUser.create(req.body).then(data => {
+            res.status(200).send(data)
+        })
     } catch {
         res.status(401).send({ msg: "There is already this cde" })
     }
 })
 
-router.get('/login/token', async (req, res) => {
+router.get('/login/token', (req, res) => {
     try {
-        const userData = await useUser.read_token(req.headers.authorization);
-        res.status(200).send(userData)
+        useUser.read_token(req.headers.authorization).then(data => {
+            res.status(200).send(data)
+        })
     } catch {
         res.status(401).send({ msg: 'There is no token' })
     }
 })
 
-router.get('/login/notToken/:login/:psswrd', async (req, res) => {
-    const token = await useUser.read_login(req.params.login, req.params.psswrd);
-    if (token === '') {
-        res.status(401).send({ msg: 'Invalid login' })
-    } else {
-        const userData = await useUser.read_token(token);
-        res.status(200).send({ user: userData, token: token })
-    }
+router.get('/login/notToken/:login/:psswrd', (req, res) => {
+    useUser.read_login(req.params.login, req.params.psswrd).then(token => {
+        if (token === '') {
+            res.status(401).send({ msg: 'Invalid login' })
+        } else {
+            useUser.read_token(token).then(userData => {
+                res.status(200).send({ user: userData, token: token })
+            })
+        }
+    })
 })
 
-router.post('/reset/psswrd', async (req, res) => {
+router.post('/reset/psswrd', (req, res) => {
     try {
         const body = req.body
-        const response = await useUser.setResetPassword(body.cde);
-        const token = response.token
-        // await sendToken(token, response.number)
-        console.log(token);
-        res.status(200).send({ msg: 'everything is okay' })
+        useUser.setResetPassword(body.cde).then(response => {
+            const token = response.token
+            // await sendToken(token, response.number)
+            console.log(token);
+            res.status(200).send({ msg: 'everything is okay' })
+        })
     } catch {
         res.status(404).send({ msg: 'unable to do it' })
     }
 })
 
-router.patch('/update/:newPsswrd/:token', async (req, res) => {
+router.patch('/update/:newPsswrd/:token', (req, res) => {
     const user = req.body
-    if (await useUser.checkChangePsswrd(user.cde, req.params.token)) {
-        await useUser.changePassword(user.cde, req.params.newPsswrd);
-        res.status(200).send({msg:'Changed! Try to log in again!'})
-    } else {
-        res.status(401).send({msg:'Unable to change, invalid token'});
-    }
+    useUser.checkChangePsswrd(user.cde, req.params.token).then(data => {
+        if (data) {
+            useUser.changePassword(user.cde, req.params.newPsswrd);
+            res.status(200).send({ msg: 'Changed! Try to log in again!' })
+        } else {
+            res.status(401).send({ msg: 'Unable to change, invalid token' });
+        }
+    })
 })
 
 module.exports = app => app.use('/auth', router);
